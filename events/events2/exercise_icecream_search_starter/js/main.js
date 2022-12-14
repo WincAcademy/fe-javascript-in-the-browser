114616cc5c9348aca131456911d9475762ba0d06
Niels Bom <niels@nielsbom.com>
2022-12-14 13:57:52 +0100

    Add starter for icecream_search

---
+++ events/events2/exercise_icecream_search_starter/js/main.js
@@ -0,0 +1,17 @@
+/*
+Events:
+* input field is clicked into/out of
+* user types
+
+State areas:
+* input has focus
+* updateSearchState
+
+Effects:
+1. input field has background color
+2. "", ✅ or ❌ before input
+3. results div is shown/hidden
+4. results div shows "found" or "not found"
+5. results div shows flavour
+*/
+const flavours = ["chocolate", "vanilla", "strawberry", "lemon"];
